f6c5724888f82c78efc217c595e8c21c80156209
Garrett D'Amore <garrett@damore.org>
2017-07-12 11:13:35 -0700

    License under MIT, use WebKit style, and add sublime project.

--- analytics.h
+++ analytics.h
@@ -1,76 +1,85 @@
-#include <string>
+//
+// Copyright 2017 Segment Inc. <friends@segment.com>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
 #include <map>
+#include <string>
 
 namespace segment {
 
 class Response {
 public:
-  Response();
-  ~Response();
+    Response();
+    ~Response();
 
-  bool ok;
-  int status;
-  std::string data;
+    bool ok;
+    int status;
+    std::string data;
 };
 
 typedef enum {
-  EVENT_TYPE_IDENTIFY,
-  EVENT_TYPE_TRACK,
-  EVENT_TYPE_PAGE,
-  EVENT_TYPE_SCREEN,
-  EVENT_TYPE_GROUP,
-  EVENT_TYPE_ALIAS
+    EVENT_TYPE_IDENTIFY,
+    EVENT_TYPE_TRACK,
+    EVENT_TYPE_PAGE,
+    EVENT_TYPE_SCREEN,
+    EVENT_TYPE_GROUP,
+    EVENT_TYPE_ALIAS
 } EventType;
 
 class Event {
 public:
-  Event (EventType type);
-  ~Event ();
+    Event(EventType type);
+    ~Event();
 
-  std::string Serialize();
-  std::string Type();
+    std::string Serialize();
+    std::string Type();
 
-  std::string userId;
-  std::string event;
-  std::string groupId;
-  std::string anonymousId;
-  std::string previousId;
-  std::map<std::string, std::string> properties;
+    std::string userId;
+    std::string event;
+    std::string groupId;
+    std::string anonymousId;
+    std::string previousId;
+    std::map<std::string, std::string> properties;
 
 private:
-  EventType type;
+    EventType type;
 };
 
 class Analytics {
 public:
-  Analytics (std::string writeKey);
-  Analytics (std::string writeKey, std::string host);
-  ~Analytics ();
+    Analytics(std::string writeKey);
+    Analytics(std::string writeKey, std::string host);
+    ~Analytics();
 
-  void Track (std::string userId, std::string event, std::map<std::string, std::string> properties);
-  void Track (std::string userId, std::string event);
+    void Track(std::string userId, std::string event, std::map<std::string, std::string> properties);
+    void Track(std::string userId, std::string event);
 
-  void Identify (std::string userId, std::map<std::string, std::string> traits);
-  void Identify (std::string userId);
+    void Identify(std::string userId, std::map<std::string, std::string> traits);
+    void Identify(std::string userId);
 
-  void Page (std::string event, std::string userId, std::map<std::string, std::string> properties);
-  void Page (std::string event, std::string userId);
+    void Page(std::string event, std::string userId, std::map<std::string, std::string> properties);
+    void Page(std::string event, std::string userId);
 
-  void Screen (std::string event, std::string userId, std::map<std::string, std::string> properties);
-  void Screen (std::string event, std::string userId);
+    void Screen(std::string event, std::string userId, std::map<std::string, std::string> properties);
+    void Screen(std::string event, std::string userId);
 
-  void Alias (std::string previousId, std::string userId);
+    void Alias(std::string previousId, std::string userId);
 
-  void Group (std::string groupId, std::map<std::string, std::string> properties);
-  void Group (std::string groupId);
+    void Group(std::string groupId, std::map<std::string, std::string> properties);
+    void Group(std::string groupId);
 
 private:
-  std::string writeKey;
-  std::string host;
-  std::string userUage;
+    std::string writeKey;
+    std::string host;
+    std::string userUage;
 
-  Response *sendEvent (Event *e);
-  static size_t sendEventWriteCallback (void *data, size_t size, size_t nmemb, void *userdata);
+    Response* sendEvent(Event* e);
+    static size_t sendEventWriteCallback(void* data, size_t size, size_t nmemb, void* userdata);
 };
 
 } // segment
