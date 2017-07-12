e9446a4330e8d32ccfdcfdd3e510abc15d14d292
Garrett D'Amore <garrett@damore.org>
2017-07-12 09:49:43 -0700

    Hard code my test key, and send messages to Segment directly.

--- example.cpp
+++ example.cpp
@@ -3,7 +3,10 @@
 using namespace segment;
 
 int main () {
-  Analytics *analytics = new Analytics("xxx", "http://localhost:55005");
+  //Analytics *analytics = new Analytics("xxx", "http://localhost:55005");
+  auto writeKey = "LpSP8WJmW312Z0Yj1wluUcr76kd4F0xl";
+  auto apiHost = "https://api.segment.io";
+  Analytics *analytics = new Analytics(writeKey, apiHost);
   analytics->Track("userId", "Did Something", { { "foo", "bar" }, { "qux", "mux" }});
   delete analytics;
   return 0;
