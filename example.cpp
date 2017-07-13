0a6c2a0f358a4eca573a445454501aec510dad59
Garrett D'Amore <garrett@damore.org>
2017-07-13 13:31:28 -0700

    More RAII -- the library and tests are now free of all leaks.
    While here, add the boilperlate to the example file.

--- example.cpp
+++ example.cpp
@@ -1,13 +1,21 @@
+//
+// Copyright 2017 Segment Inc. <friends@segment.com>
+//
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
 #include "analytics.h"
 
 using namespace segment;
 
-int main () {
-  //Analytics *analytics = new Analytics("xxx", "http://localhost:55005");
-  auto writeKey = "LpSP8WJmW312Z0Yj1wluUcr76kd4F0xl";
-  auto apiHost = "https://api.segment.io";
-  Analytics *analytics = new Analytics(writeKey, apiHost);
-  analytics->Track("userId", "Did Something", { { "foo", "bar" }, { "qux", "mux" }});
-  delete analytics;
-  return 0;
+int main()
+{
+    auto writeKey = "<your-custom-write-key-here>";
+    auto apiHost = "https://api.segment.io";
+    Analytics analytics(writeKey, apiHost);
+    analytics.Track("userId", "Did Something", { { "foo", "bar" }, { "qux", "mux" } });
+    return 0;
 }
--- tests/test-submit.cpp
+++ tests/test-submit.cpp
@@ -1,6 +1,12 @@
 //
-// XXX: Insert BoilerPlate Here.
+// Copyright 2017 Segment Inc. <friends@segment.com>
 //
+// This software is supplied under the terms of the MIT License, a
+// copy of which should be located in the distribution where this
+// file was obtained (LICENSE.txt).  A copy of the license may also be
+// found online at https://opensource.org/licenses/MIT.
+//
+
 #include "analytics.h"
 
 #include <curl/curl.h> // So that we can clean up memory at the end.
@@ -16,11 +22,11 @@ TEST_CASE("Submissions to Segment work", "[analytics]")
     {
         auto writeKey = "LpSP8WJmW312Z0Yj1wluUcr76kd4F0xl";
         auto apiHost = "https://api.segment.io";
-        Analytics* analytics = new Analytics(writeKey, apiHost);
+        Analytics analytics(writeKey, apiHost);
 
         THEN("we can submit tracked events")
         {
-            REQUIRE_NOTHROW(analytics->Track("userId", "Did Something",
+            REQUIRE_NOTHROW(analytics.Track("userId", "Did Something",
                 { { "foo", "bar" }, { "qux", "mux" } }));
         }
     }
@@ -29,12 +35,12 @@ TEST_CASE("Submissions to Segment work", "[analytics]")
     {
         auto writeKey = "LpSP8WJmW312Z0Yj1wluUcr76kd4F0xl";
         auto apiHost = "https://api.segment.io/nobodyishome";
-        Analytics* analytics = new Analytics(writeKey, apiHost);
+        Analytics analytics(writeKey, apiHost);
 
         THEN("gives a 404 HttpException")
         {
             REQUIRE_THROWS_WITH(
-                analytics->Track("userId", "Did Something",
+                analytics.Track("userId", "Did Something",
                     { { "foo", "bar" }, { "qux", "mux" } }),
                 Catch::Contains("404"));
         }
@@ -60,12 +66,12 @@ TEST_CASE("Submissions to Segment work", "[analytics]")
     {
         auto writeKey = "LpSP8WJmW312Z0Yj1wluUcr76kd4F0xl";
         auto apiHost = "https://localhost:50051";
-        Analytics* analytics = new Analytics(writeKey, apiHost);
+        Analytics analytics(writeKey, apiHost);
 
         THEN("Connection is refused")
         {
             REQUIRE_THROWS_WITH(
-                analytics->Track("userId", "Did Something",
+                analytics.Track("userId", "Did Something",
                     { { "foo", "bar" }, { "qux", "mux" } }),
                 "Connection refused");
         }
